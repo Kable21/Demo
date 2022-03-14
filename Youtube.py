ff74c339f1217590393df1c2ad8f616037c6c9c8
kable <kablely21@gmail.com>
2022-03-14 23:43:58 +0000

    1 hour python tutorial 1:18:57

--- Youtube.py
+++ Youtube.py
@@ -1,1 +1,14 @@
-print("Giraffe \nAcademy")
+# num1 = input("Enter a number: ")
+# num2 = input("Enter another number: ")
+# result = float(num1) + float(num2)
+# print(result)
+# color = input("Enter a color: ")
+# plural_noun = input("Enter a plural_noun: ")
+# celebrity = input("Enter a celebrity: ")
+# print("Rose are" + color)
+# print(plural_noun + "are blue")
+# print("I love" + celebrity)
+lucky_numbers = [4,8,15,16,23,42]
+friends = ["Kevin", "Keran", "Jim", "Oscar", "Tom"]
+friends2 = friends.copy()
+print(friends2)
